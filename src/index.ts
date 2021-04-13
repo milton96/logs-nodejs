d7cc2093d1b3946771beaea3ff16f052e740fe27
Milton Bravo <miltonbravo96@gmail.com>
2021-04-12 22:51:28 -0500

    leer log

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Log } from "./log";
 console.log("LOGS TS");
 
-Log.add(Log.OK, "Log con OK");
-Log.add(Log.INFO, "Log con INFO");
-Log.add(Log.WARNING, "Log con WARNING");
-Log.add(Log.ERROR, "Log con ERROR");
+// Log.add(Log.OK, "Log con OK");
+// Log.add(Log.INFO, "Log con INFO");
+// Log.add(Log.WARNING, "Log con WARNING");
+// Log.add(Log.ERROR, "Log con ERROR");
+
+Log.read("2021-04-12");
--- src/log.ts
+++ src/log.ts
@@ -1,11 +1,15 @@
 import fs from "fs";
 import path from "path";
+import moment from "moment";
 
 export class Log {
     public static OK: String = "OK";
     public static INFO: String = "INFO";
     public static WARNING: String = "WARNING";
     public static ERROR: String = "ERROR";
+
+    private static FECHA: String = "YYYY-MM-DD";
+    private static FECHAHORA: String = "YYYY-MM-DD HH:mm:ss";
 
     /**
      * Crear un log y lo agrega al archivo
@@ -14,8 +18,8 @@ export class Log {
      */
     public static async add(type: String, info: String) {
         const date: Date = new Date();
-        const text: String = `${date.toLocaleString()}: ${type} - ${info}\n`;
-        const filename: String = `${date.toISOString().split("T")[0]}.txt`;
+        const text: String = `${moment(date).format(this.FECHAHORA.toString())}: ${type} - ${info}\n`;
+        const filename: String = `${moment(date).format(this.FECHA.toString())}.txt`;
         const logs: String = "/logs/";
         const base_path: String = path.join(__dirname, logs.toString());
         const ruta: fs.PathLike = path.join(base_path.toString(), filename.toString());
@@ -41,5 +45,25 @@ export class Log {
         } catch (error) {
             console.error(error);
         }
+    }
+
+    /**
+     * read
+     * @param filename nombre del archivo
+     */
+    public static read(filename: String) {
+        if (!filename.endsWith(".txt")) filename += ".txt";
+        
+        const logs: String = "/logs/";
+        const base_path: String = path.join(__dirname, logs.toString());
+        const ruta: fs.PathLike = path.join(base_path.toString(), filename.toString());
+
+        fs.readFile(ruta, 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(data);
+            }
+        });
     }
 }
